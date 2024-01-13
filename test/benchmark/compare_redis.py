6a72fb14cc1880e98fa4457a37df9acef0032472
B777B2056 <1937343022@qq.com>
2024-01-13 22:07:15 +0800

    添加key删除性能对比实验

--- test/benchmark/compare_redis.py
+++ test/benchmark/compare_redis.py
@@ -55,6 +55,21 @@ def BenchmarkGet(clt: redis.client.Redis) -> List[float]:
     return ret
 
 
+def BenchmarkDel(clt: redis.client.Redis) -> List[float]:
+    def BenchmarkDelOnce(s: str) -> float:
+        start = time.time() * 1000
+        clt.delete(s)
+        return time.time() * 1000 - start
+
+    ret = []
+    for data in TestDataSet:
+        t = 0.0
+        for _ in range(0, IterationNum):
+            t += BenchmarkDelOnce(data)
+        ret.append(t / IterationNum)
+    return ret
+
+
 class Plotter:
     fig: plt.Figure
     subIdx: int
@@ -62,10 +77,11 @@ class Plotter:
     class Mode(Enum):
         FoxbatDB_Redis_Read = 1
         FoxbatDB_Redis_Write = 2
+        FoxbatDB_Redis_Delete = 3
 
     def __init__(self):
         self.fig = plt.figure()
-        self.subIdx = 121
+        self.subIdx = 221
         plt.rcParams['font.sans-serif'] = ['SimHei']
 
     def __Title(self, mode: Mode) -> str:
@@ -73,10 +89,12 @@ class Plotter:
             return '读性能对比（FoxbatDB vs Redis）'
         elif mode == Plotter.Mode.FoxbatDB_Redis_Write:
             return '写性能对比（FoxbatDB vs Redis）'
+        else:
+            return '删除性能对比（FoxbatDB vs Redis）'
 
     def PerformanceComparisonWithRedis(self, mode: Mode, foxbatDBTimeList: List[float],
                                        redisTimeList: List[float]) -> None:
-        assert self.subIdx == 121 or self.subIdx == 122
+        assert self.subIdx == 221 or self.subIdx == 222 or self.subIdx == 223
         _ = self.fig.add_subplot(self.subIdx)
         plt.title(self.__Title(mode))
         plt.xlabel('key/value长度（字节）')
@@ -84,12 +102,12 @@ class Plotter:
         l1, = plt.plot(StrSizeList, foxbatDBTimeList, marker='o', markersize=3)
         l2, = plt.plot(StrSizeList, redisTimeList, marker='x', markersize=3)
         plt.legend(handles=[l1, l2], labels=['FoxbatDB', 'Redis'], loc='best')
-        if self.subIdx == 122:
-            plt.show()
         self.subIdx += 1
 
     def PerformanceComparisonWithReadWrite(self, foxbatDBSetTimeList: List[float],
                                            foxbatDBGetTimeList: List[float]) -> None:
+        assert self.subIdx == 224
+        _ = self.fig.add_subplot(self.subIdx)
         plt.title('读写性能对比（FoxbatDB）')
         plt.xlabel('key/value长度（字节）')
         plt.ylabel('耗时（毫秒）')
@@ -110,12 +128,15 @@ if __name__ == '__main__':
     # 获取测量数据
     foxbatDBSetTimeList = BenchmarkSet(foxbatDBClient)
     foxbatDBGetTimeList = BenchmarkGet(foxbatDBClient)
+    foxbatDBDelTimeList = BenchmarkDel(foxbatDBClient)
 
     redisSetTimeList = BenchmarkSet(redisClient)
     redisGetTimeList = BenchmarkGet(redisClient)
+    redisDelTimeList = BenchmarkDel(redisClient)
 
     # 绘图
     plotter = Plotter()
     plotter.PerformanceComparisonWithRedis(Plotter.Mode.FoxbatDB_Redis_Write, foxbatDBSetTimeList, redisSetTimeList)
     plotter.PerformanceComparisonWithRedis(Plotter.Mode.FoxbatDB_Redis_Read, foxbatDBGetTimeList, redisGetTimeList)
+    plotter.PerformanceComparisonWithRedis(Plotter.Mode.FoxbatDB_Redis_Delete, foxbatDBDelTimeList, redisDelTimeList)
     plotter.PerformanceComparisonWithReadWrite(foxbatDBSetTimeList, foxbatDBGetTimeList)
